47a2bd1e649683f4e73e3b871a9ac1a02fd6f707
FaiqRN <faiqramzy2004@gmail.com>
2024-03-28 23:28:19 +0700

    first commit

---
+++ Jobsheet 8/1.2/form_upload1.2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>File Upload</title>
+</head>
+<body>
+    <h2>Upload File</h2>
+    <form action="upload1.2.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload File" name="submit">
+    </form>
+</body>
+</html>
